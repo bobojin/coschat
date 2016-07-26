c4208546aef46df7115d7581e6bcd06276cad74b
bobojin <boboy3@gmail.com>
2016-07-26 09:21:18 +0800

    Remove tab

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 	
 	echo "<html lang='zh-cn'>\n";
 	echo "<head>\n";
-	echo "\t<meta charset='UTF-8'>\n";
-	echo "\t<meta content='IE=edge' http-equiv='X-UA-Compatible'>\n";
-	echo "\t<title>CosChat</title>\n";
+	echo "<meta charset='UTF-8'>\n";
+	echo "<meta content='IE=edge' http-equiv='X-UA-Compatible'>\n";
+	echo "<title>CosChat</title>\n";
 	echo "</head>\n";
 	echo "<body>\n";
 	
@@ -12,7 +12,7 @@
 	
 	$result = mysql_query("SELECT * FROM channel");
 	while($row = mysql_fetch_array($result)){
-		echo "\t\t\t<p>" . $row['channel_name'] . "</p>\n";
+		echo "<p>" . $row['channel_name'] . "</p>\n";
 	}
 	
 	echo "</body>\n";
